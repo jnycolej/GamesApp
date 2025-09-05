b1b509a80195d36a38cd1e101bcc4f95b7f8d8c4
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-09-05 11:21:09 -0500

    Removed unnecessary imports

--- frontend/src/pages/FootballGameMainPage.js
+++ frontend/src/pages/FootballGameMainPage.js
@@ -1,27 +1,13 @@
 import React, {useState} from "react";
 import NavBar from "../components/NavBar";
-import jsonData from '../assets/deck.json';
+import jsonData from '../assets/footballDeck.json';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (optional)
 // import { useNavigate } from "react-router-dom";
-//import bg from '../assets/baseball-background.png';
-
-
-import {Howl} from 'howler';
 
 const FootballGameMainPage = () => {
     const [hand, setHand] = useState([]);
     const [points, setPoints] = useState(0);
-
-    const containerStyle = {
-    //backgroundImage: `url(${bg})`,
-    minHeight: '100vh',
-    width: '100%',
-    backgroundRepeat: 'no-repeat',
-    backgroundAttachment: 'fixed',
-    backgroundSize: 'cover',
-  };
-   //const navigate = useNavigate();
 
     const generateCard = () => {
         const randomNum = Math.floor(Math.random() * (jsonData.length - 0) + 0);
@@ -36,23 +22,13 @@ const FootballGameMainPage = () => {
         return newHand;
     }
 
-    // useEffect(() => {
-    //     setHand(generateHand());
-    // }, []);
-
     const handlePlayBall = () => {
         setHand(generateHand());
     }
 
-    // const hitSound = new Howl({
-    //     src:['sounds/bat-hit.mp3', 'sounds/bat-hit.ogg'],
-    //     volume: 0.7
-    // });
-
     const handleCardClick = (index, cardPoints) => {
         
         setPoints(prev => prev + cardPoints);
-        // hitSound.play();
         const newCard = generateCard();
 
         setHand(prevHand => {
@@ -69,15 +45,18 @@ const FootballGameMainPage = () => {
 
     return(
         <div className="containerStyle">
+            <div className="football-background">
             <h1>Football Game</h1>
             <NavBar />
+            <h2>Create Room</h2>
+            <button className="btn btn-success">Create Room</button>
             <h3>Deck | Points: {points}</h3>
             <button className="btn btn-danger" onClick={handlePlayBall}>Play ball</button>
             <button className="btn btn-secondary" onClick={restartGame}>Reset</button>
             <div className="container">
                 <div className="row">
                     {hand.map((card, idx) => (
-                        <div className="col m-2 h-50" key={hand.id}>
+                        <div className="col m-2" key={hand.id}>
                             <button className="card playingCard p-3" onClick={() => handleCardClick(idx, card.points)}>
                                 <p className="card-text">{card.description}</p>
                                 <p className="card-text">{card.penalty}</p>
@@ -87,7 +66,9 @@ const FootballGameMainPage = () => {
                     ))}                    
                 </div>
                 
+            </div>                
             </div>
+
             
 
         </div>
