b2f449ec4cfec61fad5278a81556ddc180dac5f7
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-09-05 11:17:03 -0500

    Added routes for creating multiplayer rooms for online multiplayer gameplay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,22 @@ import HomePage from './pages/HomePage';
 import BaseballGameMainPage from './pages/BaseballGameMainPage';
 import FootballGameMainPage from './pages/FootballGameMainPage';
 
+import ModeSelect from './player/ModeSelect';
+import JoinCreateRoom from './player/JoinCreateRoom';
+import GameLobby from "./player/GameLobby";
+import GameScreen from "./player/GameScreen";
+
 const router = createBrowserRouter(
   [
     {path: "/", element: <HomePage />},
+
     {path: "/baseball-home", element: <BaseballGameMainPage />},
     {path: "/football-home", element: <FootballGameMainPage />},
+
+    { path: '/:game/mode', element: <ModeSelect />},
+    { path: '/:game/join', element: <JoinCreateRoom />},
+    { path: '/:game/lobby/:code', element: <GameLobby />},
+    { path: '/:game/game', element: <GameScreen />},
   ]
 );
 
