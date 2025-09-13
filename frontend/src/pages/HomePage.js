32f31fb8d5c06964acf48b94a3bbe7def02b0fe0
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-09-13 14:44:39 -0500

    Updated the Homepage user interface

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,9 +4,11 @@ import NavBar from '../components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (optional)
 
+import baseballGameLogo from '../assets/baseballgamecardlogo.png';
+import footballGameLogo from '../assets/football-card-logo.png'
 const HomePage = () => {
 
-const navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
         <div className="home-background">
@@ -14,13 +16,32 @@ const navigate = useNavigate();
             <NavBar />
             <div className="container d-grid row-gap-3">
                 <h1 className="display-4 text-center">Games</h1>
-                <div className="">
-                    <Link to="/baseball/mode"><button className="btn btn-lg btn-warning " onClick={() => navigate('/baseball-home')}>Baseball</button></Link>
-                    <Link to="/football/mode"><button className="btn btn-lg btn-success" onClick={() => navigate('/football-home')}>Football</button></Link>                    
-                </div>
-
             </div>
+            <div class="container">
+                <div class="mb-3 row justify-content-start gap-4 text-center">
+                    <div class="card col home-card mb-4 shadow-lg">
+                        <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">Baseball Game</h4>
+                        </div>
+                        <div className="baseball-home-card-logo">
+                            <div class="card-body">
+                                <Link to="/baseball/mode"><img className="card-img-top" src={baseballGameLogo} alt="Baseball Game"></img></Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col home-card mb-4 shadow-lg">
+                        <div class="card-header">
+                            <h4 class="my-0 font-weight-normal">Football Game</h4>
+                        </div>
+                        <div className="football-home-card-logo">
+                            <div class="card-body">
+                                <Link to="/football/mode"><img className="card-img-top" src={footballGameLogo} alt="Football Game"></img></Link>
+                            </div>
+                        </div>
 
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
