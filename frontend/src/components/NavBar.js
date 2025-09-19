f5f840be1a89d2ec780bc6e29dafe9157ede9688
Jennifer Joseph <98425262+jnycolej@users.noreply.github.com>
2025-09-19 15:59:41 +0000

    Updated Navbar and updated font

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,21 @@
 import React from "react";
-import {useNavigate, Link} from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (optional)
 
 const NavBar = () => {
     const navigate = useNavigate();
-    
+
     return (
         <div className="m-2">
-            <button className="btn graduate-regular text-light" onClick={() => navigate('/')}>Home</button>
-            <Link to="/baseball/mode"><button className="btn text-light" onClick={() => navigate('/baseball-home')}>Baseball Game</button></Link>
-            <Link to="/football/mode"><button className="btn text-light" onClick={() => navigate('/football-home')}>Football Game</button></Link>            
+            <nav className="navbar navbar-expand-lg">
+                <a className="navbar-brand graduate-regular">Sports Shuffle</a>
+                <div className="navbar-nav">
+                <button className="btn graduate-regular text-light" onClick={() => navigate('/')}>Home</button>
+                <Link to="/baseball/mode"><button className="nav-link graduate-regular text-light" onClick={() => navigate('/baseball-home')}>Baseball Game</button></Link>
+                <Link to="/football/mode"><button className="nav-link graduate-regular text-light" onClick={() => navigate('/football-home')}>Football Game</button></Link>
+                </div>
+            </nav>
         </div>
 
     )
