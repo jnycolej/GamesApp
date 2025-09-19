585a01b768b7d075bcb55bc4a4e46c106e5b3587
Jennifer Joseph <98425262+jnycolej@users.noreply.github.com>
2025-09-19 17:15:00 +0000

    updated navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,13 +8,25 @@ const NavBar = () => {
 
     return (
         <div className="m-2">
-            <nav className="navbar navbar-expand-md">
-                <div className="navbar-nav">
-                <button className="btn graduate-regular text-light" onClick={() => navigate('/')}>Home</button>
-                <Link to="/baseball/mode"><button className="nav-link graduate-regular text-light" onClick={() => navigate('/baseball-home')}>Baseball Game</button></Link>
-                <Link to="/football/mode"><button className="nav-link graduate-regular text-light" onClick={() => navigate('/football-home')}>Football Game</button></Link>
+            <nav className="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <a className="navbar-brand graduate-regular" href="/">Sports Shuffle</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/baseball/mode"><a className="nav-link graduate-regular text-light" onClick={() => navigate('/baseball-home')}>Baseball Game</a></Link>
+                            </li>
+                            <li className="nav-item"><Link to="/baseball/mode"><a className="nav-link graduate-regular text-light" onClick={() => navigate('/football/home')}>Football Game</a></Link></li>
+
+                        </ul>
+                    </div>
                 </div>
-                <a className="navbar-brand graduate-regular">Sports Shuffle</a>
             </nav>
         </div>
 
