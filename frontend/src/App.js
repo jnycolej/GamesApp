2f0ab81800ecd874d8366aa2bde0686706c3636d
Jennifer Joseph <jennifer.n.joseph@gmail.com>
2025-10-24 12:11:39 -0500

    Added comments for code clarity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,17 +10,21 @@ import JoinCreateRoom from './player/JoinCreateRoom';
 import GameLobby from "./player/GameLobby";
 import GameScreen from "./player/GameScreen";
 
+import TriviaQuiz from './components/TriviaQuiz';
+
 const router = createBrowserRouter(
   [
     {path: "/", element: <HomePage />},
 
-    {path: "/baseball-home", element: <BaseballGameMainPage />},
-    {path: "/football-home", element: <FootballGameMainPage />},
+    {path: "/baseball-home", element: <BaseballGameMainPage />},  //Baseball single player homepage
+    {path: "/football-home", element: <FootballGameMainPage />},  //Football single player homepage
 
-    { path: '/:game/mode', element: <ModeSelect />},
-    { path: '/:game/join', element: <JoinCreateRoom />},
-    { path: '/:game/lobby/:code', element: <GameLobby />},
-    { path: '/:game/game', element: <GameScreen />},
+    { path: '/:game/mode', element: <ModeSelect />},              //Select between single and multiplayer
+    { path: '/:game/join', element: <JoinCreateRoom />},          //Join or Create room for multiplayer page
+    { path: '/:game/lobby/:code', element: <GameLobby />},        //Multiplayer game lobby page
+    { path: '/:game/game', element: <GameScreen />},              //Multiplayer game screen
+    
+    { path: '/trivia', element: <TriviaQuiz />}                   // Trivia Quiz (Coming Soon)
   ]
 );
 
