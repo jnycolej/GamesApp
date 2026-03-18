323a00413109b854bf3e9fcb2ebe410e26a1c574
Jennifer Joseph <jennifer.n.joseph@gmail.com>
2026-03-18 11:09:07 -0500

    General UI updates

--- frontend-vite/src/main.jsx
+++ frontend-vite/src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (optional)
+
 import './assets/App.css';                             
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
