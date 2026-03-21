f38fdfdc117796627387882a7c47e2d1271fa069
Jennifer Joseph <jennifer.n.joseph@gmail.com>
2026-03-21 13:56:10 -0500

    Added a object to track team info

---
+++ frontend-vite/src/assets/data/teams.js
@@ -0,0 +1,88 @@
+export const collegeTeams = [
+  {
+    id: "lsu",
+    schoolName: "lousiana state university",
+    shortName: "LSU",
+    aliases: [
+      "LSU",
+      "LSU Tigers",
+      "Louisiana State University Tigers",
+      "Fighting Tigers",
+      "Bayou Bengals",
+    ],
+    mascot: "tigers",
+    colors: {
+      primary1: "#461D7C",
+      primary2: "#FDD023",
+    },
+    league: "NCAA",
+    conference: "SEC",
+    sports: ["football", "baseball", "basketball"],
+    location: { city: "New Orleans", state: "Louisiana" },
+    rivalries: ["alabama", "florida", "ole-miss"],
+  },
+  {
+    id: "clemson",
+    schoolName: "clemson university",
+    shortName: "clem",
+    aliases: ["Clemson", "Clemson Tigers", "Clemson University Tigers"],
+    mascot: "tigers",
+    colors: {
+      primary1: "#F56600",
+      primary2: "#522D80",
+    },
+    league: "NCAA",
+    conference: "ACC",
+    sports: ["football", "baseball"],
+    location: {
+      city: "Clemson",
+      state: "South Carolina",
+    },
+    rivalries: [],
+  },
+  {
+    id: "uark",
+    schoolName: "university of arkansas",
+    shortName: "uark",
+    aliases: [
+      "Arkansas",
+      "Arkansas Razorbacks",
+      "University of Arkansas Razorbacks",
+      "Razorbacks",
+    ],
+    mascot: "razorbacks",
+    colors: {
+      primary1: "#F56600",
+      primary2: "#522D80",
+    },
+    league: "NCAA",
+    conference: "SEC",
+    sports: ["football", "baseball"],
+    location: {
+      city: "fayetteville",
+      state: "arkansas",
+    },
+    rivalries: [],
+  },
+  {
+    id: "uncbears",
+    schoolName: "university of northern colorado",
+    shortName: "clem",
+    aliases: ["Bears", "university of colorado bears", "colorado bears", "north colorado"],
+    mascot: "bears",
+    colors: {
+      primary1: "#013C65",
+      primary2: "#F6B000",
+    },
+    league: "NCAA",
+    conference: "Summit League",
+    sports: ["baseball"],
+    location: {
+      city: "Greeley",
+      state: "Colorado",
+    },
+    rivalries: [],
+  },
+];
+
+export const professionalTeams = [];
